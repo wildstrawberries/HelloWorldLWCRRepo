0f18bd8545f60d5a8d925b2e39610ae448616f35
Dirk Koechner <dirk.koechner@slalom.com>
2023-09-22 10:37:05 +0200

    first commit

---
+++ force-app/main/default/lwc/lWC1HelloWorld/lWC1HelloWorld.js
@@ -0,0 +1,55 @@
+import { LightningElement, track } from 'lwc';
+import getAccounts1 from '@salesforce/apex/myAccountListController.getAccounts';
+
+export default class ApexImperativeMethod extends LightningElement {
+
+  draftValues = [];
+
+  accounts = [];
+  accountList = [];
+  columnList = [
+    { label: 'ID', fieldName: 'Id' },
+    { label: 'Name', fieldName: 'Name' },
+    { label: 'AccountNumber', fieldName: 'AccountNumber' },
+    { label: 'Notes', fieldName: 'Notes', editable: true }
+  ];
+  noRecordsFound = false;
+
+  greeting = 'World';
+  greetingHandler(event) {
+    this.greeting = event.target.value;
+  }
+
+  pullAccounts() {
+    this.accName = this.template.querySelector("lightning-input[data-recid=input_accName]").value;
+    getAccounts1({ strAccountName : this.accName })
+      .then((result) => {
+        this.accountList = result;
+      }); 
+  }
+
+  saveAction(event) {
+    var selectedRecords =  this.template.querySelector("lightning-datatable").getSelectedRows();
+
+    this.draftValues = event.detail.draftValues;
+    console.log("LOG: draftValues");
+    console.log(this.draftValues);
+    console.log("LOG: selectedRecords");
+    console.log(selectedRecords);
+
+    if(selectedRecords.length > 0){
+      this.accounts = selectedRecords;
+
+      selectedRecords.forEach(function(element){
+          console.log(element.Name);
+      });
+
+      this.draftValues.forEach(function(element){
+        console.log(element.id);
+        console.log(element.Notes);
+    });
+
+    }   
+  }
+
+}
